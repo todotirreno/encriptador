812c39944f5daaf8663f49a382b08effc54e20ff
todotirreno <16512686+todotirreno@users.noreply.github.com>
2023-01-23 16:20:07 -0300

    Add decrypt function

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 "use strict";
 
-const button = document.getElementById("encrypt");
-button.addEventListener("pointerdown", () => {
+const encryptButton = document.getElementById("encrypt");
+const decryptButton = document.getElementById("decrypt");
+
+encryptButton.addEventListener("pointerdown", encrypt);
+decryptButton.addEventListener("pointerdown", decrypt);
+
+function encrypt() {
   const input = document.getElementById("input").value;
   if (!validate(input)) return;
 
-  const encryption = { a: "ai", e: "enter", i: "imes", o: "ober", u: "u" };
+  const encryption = { a: "ai", e: "enter", i: "imes", o: "ober", u: "ufat" };
   const output = input.replace(/[aeiou]/g, (char) => encryption[char]);
 
   const outputElements = document.querySelector(".output-container");
@@ -18,7 +23,9 @@ button.addEventListener("pointerdown", () => {
   container.classList.add("output");
 
   outputElements.replaceChildren(container, copy);
-});
+
+  copy.addEventListener("pointerdown", copyF);
+}
 
 function validate(str) {
   if (!str) return false;
@@ -26,4 +33,32 @@ function validate(str) {
     if ((letter >= "A" && letter < "a") || letter > "z") return false;
   }
   return true;
+}
+
+function decrypt() {
+  const input = document.getElementById("input").value;
+  if (!validate(input)) return;
+
+  const encryption = { ai: "a", enter: "e", imes: "i", ober: "o", ufat: "u" };
+  const output = input.replace(
+    /(ai)|(enter)|(imes)|(ober)|(ufat)/g,
+    (group) => encryption[group]
+  );
+
+  const outputElements = document.querySelector(".output-container");
+  const copy = document.createElement("button");
+  copy.classList.add("submit");
+  copy.append("Copiar");
+  const container = document.createElement("div");
+  container.classList.add("output-text");
+  container.append(output);
+  container.classList.add("output");
+
+  outputElements.replaceChildren(container, copy);
+  copy.addEventListener("pointerdown", copyF);
+}
+
+function copyF() {
+  const text = document.querySelector(".output-text").textContent;
+  navigator.clipboard.writeText(text);
 }
